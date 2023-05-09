88a914c579d31b2e92004ffa5974cf9e5b6c4385
maicolalmaralestamayo <maicol.almarales.tamayo@gmail.com>
2023-05-09 17:02:46 +0200

    ok

--- app/Http/Middleware/AutenticacionMiddleware.php
+++ app/Http/Middleware/AutenticacionMiddleware.php
@@ -5,12 +5,13 @@ namespace App\Http\Middleware;
 use App\Http\Controllers\TokenController;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class AutenticacionMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
+        return response(['data' => $request->body]);
+        
         if (TokenController::checkLogin($request)) {
             return $next($request);
         } else {
